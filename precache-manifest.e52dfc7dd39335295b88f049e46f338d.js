258a053bdf018058427a68174da4f8229f9b2c94
Alissa Schlecht <alissaschlecht@Cathleens-MacBook-Pro.local>
2019-10-02 19:32:24 -0400

    build

---
+++ precache-manifest.e52dfc7dd39335295b88f049e46f338d.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a50c0999e67c8fbf0e88bd94df309787",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f96b8033435e8834b2c",
+    "url": "/static/css/main.eaed9264.chunk.css"
+  },
+  {
+    "revision": "5f4c7ad5dea4ab779c80",
+    "url": "/static/js/2.b2974c81.chunk.js"
+  },
+  {
+    "revision": "4f96b8033435e8834b2c",
+    "url": "/static/js/main.5024ed31.chunk.js"
+  },
+  {
+    "revision": "fa11afcd6372d975ed76",
+    "url": "/static/js/runtime~main.98592f35.js"
+  },
+  {
+    "revision": "9fb6b3e5a819eba89141aa57d8fccbe8",
+    "url": "/static/media/AlissaSchlecht-CV.9fb6b3e5.pdf"
+  },
+  {
+    "revision": "79f84ac5ac80ec38d1118974d5caccdf",
+    "url": "/static/media/alissa-headshot.79f84ac5.jpg"
+  },
+  {
+    "revision": "d04d1db72ee7490f9e8c2e5e1dd45b7a",
+    "url": "/static/media/cat-cover.d04d1db7.png"
+  },
+  {
+    "revision": "c031604fc7aa2cb926fa663b30dd51e0",
+    "url": "/static/media/cat-screenshot.c031604f.png"
+  },
+  {
+    "revision": "0523a8313a2d3645473027d007b7a473",
+    "url": "/static/media/countries-illustrator.0523a831.jpg"
+  },
+  {
+    "revision": "a479130731726525d2b8cd6c5979766c",
+    "url": "/static/media/countries-screenshot.a4791307.jpg"
+  },
+  {
+    "revision": "d4cd504b398b1114f69ee1d3bb3e0f04",
+    "url": "/static/media/hanen-checkout.d4cd504b.jpg"
+  },
+  {
+    "revision": "b123e52b6a7cd97aa3d4ff7ace800d3f",
+    "url": "/static/media/hanen-home-full-page.b123e52b.jpg"
+  },
+  {
+    "revision": "d24c6437344b0f041bbe4b8f77523d3e",
+    "url": "/static/media/hanen-home-screenshot.d24c6437.jpg"
+  },
+  {
+    "revision": "c74c383a8bcaf08f7a977815799f92a3",
+    "url": "/static/media/hanen-programs.c74c383a.jpg"
+  },
+  {
+    "revision": "56aab15f60b65a94aa614ddf5c281a74",
+    "url": "/static/media/hanen-shop.56aab15f.jpg"
+  },
+  {
+    "revision": "00140ab039a77656adae6f2309a60cc1",
+    "url": "/static/media/hanen-training.00140ab0.jpg"
+  },
+  {
+    "revision": "582ee17a5b929c04806d3744cff38534",
+    "url": "/static/media/new-cat.582ee17a.png"
+  },
+  {
+    "revision": "341fbcfc76cb57dcb12c499d9b0b1d45",
+    "url": "/static/media/old-cat.341fbcfc.jpg"
+  },
+  {
+    "revision": "a86d742ab3d4a7e663435ababa5b3b6c",
+    "url": "/static/media/old-cat.a86d742a.png"
+  },
+  {
+    "revision": "ae580245802d847e102e72ab181137f8",
+    "url": "/static/media/proxima_nova_alt_light-webfont.ae580245.woff2"
+  },
+  {
+    "revision": "cbcfeaa08f00f6c0b0a02ce3b55d12e4",
+    "url": "/static/media/proxima_nova_alt_light-webfont.cbcfeaa0.woff"
+  },
+  {
+    "revision": "3c1dc98af32d6bd229cb5aa829cd435e",
+    "url": "/static/media/proxima_nova_bold-webfont.3c1dc98a.woff2"
+  },
+  {
+    "revision": "a47a78e226eb4cfad66ae94c6711e465",
+    "url": "/static/media/proxima_nova_bold-webfont.a47a78e2.woff"
+  },
+  {
+    "revision": "22a749be65499dabcb8d1ac1a9b691ed",
+    "url": "/static/media/proximanova-regular-webfont.22a749be.woff"
+  },
+  {
+    "revision": "aa655fd9ed7b7a6cfe73d31a40be3f04",
+    "url": "/static/media/proximanova-regular-webfont.aa655fd9.woff2"
+  },
+  {
+    "revision": "bc243896080ac77cb528d5429ba6c1b7",
+    "url": "/static/media/sticky-1.bc243896.png"
+  },
+  {
+    "revision": "ae7cf6cc50cac91b8f6bc8ce8129d12d",
+    "url": "/static/media/sticky-2.ae7cf6cc.png"
+  },
+  {
+    "revision": "baa599918de96cdef46ad19729016594",
+    "url": "/static/media/sticky-3.baa59991.png"
+  },
+  {
+    "revision": "6b29ec7de95449c5408a39a799f365cf",
+    "url": "/static/media/sticky-4.6b29ec7d.png"
+  },
+  {
+    "revision": "03a07bf86ac0c3e5332012d1365cfea1",
+    "url": "/static/media/sticky-5.03a07bf8.png"
+  },
+  {
+    "revision": "3e3b98ab8bcf74caafd7708fe992a622",
+    "url": "/static/media/sticky-6.3e3b98ab.png"
+  },
+  {
+    "revision": "c56741d94d39c9b9482bddd89671eee7",
+    "url": "/static/media/sticky-7.c56741d9.png"
+  },
+  {
+    "revision": "16cbd72c0d88abf8b93542bafc0f9356",
+    "url": "/static/media/sticky-8.16cbd72c.png"
+  },
+  {
+    "revision": "a12c03072f95d3d91af7c08a3f209da7",
+    "url": "/static/media/sticky-9.a12c0307.png"
+  }
+]);
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/reset.scss';
-import './index.scss';
-import Portfolio from './Portfolio';
-
-ReactDOM.render(<Portfolio />, document.getElementById('root'));
